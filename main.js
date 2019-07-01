5bbc9737783da6c81db95e5a0f2131d525005006
Vanessa Randall <vanessalrandall@gmail.com>
2019-06-30 23:01:28 -0600

    Removed unnecessary code

--- main.js
+++ main.js
@@ -3,8 +3,7 @@
 var nameDogBtn = document.querySelector(".dog-name-button");
 nameDogBtn.addEventListener("click",updateText);
 
-function updateText(e) {
-	e.preventDefault();
+function updateText() {
 	var dogName = document.querySelector(".dog-name-input").value;
 	var someDogsText = document.querySelector(".some-dogs");
 	someDogsText.textContent = dogName;
